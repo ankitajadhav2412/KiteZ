8f2eaa33965ae3aa900301e30c3d292bdbead301
chaferameshwar <chaferameshwar@gmail.com>
2021-05-02 13:42:42 +0530

    [RAMESHWAR]-CHANGES IN ANKITABAI

--- src/Sample/demo.java
+++ src/Sample/demo.java
@@ -2,4 +2,8 @@ package Sample;
 
 public class demo {
 
+	
+	public static void main(String[] args) {
+		System.out.println("hi ankita bai how are you do study otherwise wair for 1 year to place");
+	}
 }
